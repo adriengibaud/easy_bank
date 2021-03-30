70d1dc44c9d9bcd6ca30f593b91101f7dd8a1e48
Adriengibaud <adriengibaud@gmail.com>
2021-03-30 16:15:26 +0200

    body done

--- src/components/body/articles/Articles.jsx
+++ src/components/body/articles/Articles.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import styled from 'styled-components';
 import ArticleCard from './ArticleCard';
 import currencyImage from '../../../assets/articles/image-currency.jpg';
+import confettiImage from '../../../assets/articles/image-confetti.jpg';
+import planeImage from '../../../assets/articles/image-plane.jpg';
+import restaurantImage from '../../../assets/articles/image-restaurant.jpg';
 
 const Articles = () => (
   <ArticlesContainer>
@@ -15,25 +18,25 @@ const Articles = () => (
   forced to only receive money in a single …"
       />
       <ArticleCard
-        image={currencyImage}
-        author="Claire Robinson"
-        title="Receive money in any currency with no fees"
-        text="The world is getting smaller and we’re becoming more mobile. So why should you be 
-  forced to only receive money in a single …"
+        image={restaurantImage}
+        author="Wilson Hutton"
+        title="Treat yourself without worrying about money"
+        text="Our simple budgeting feature allows you to separate out your spending and set 
+  realistic limits each month. That means you …"
       />
       <ArticleCard
-        image={currencyImage}
-        author="Claire Robinson"
-        title="Receive money in any currency with no fees"
-        text="The world is getting smaller and we’re becoming more mobile. So why should you be 
-  forced to only receive money in a single …"
+        image={planeImage}
+        author="Wilson Hutton"
+        title="Take your Easybank card wherever you go"
+        text=" We want you to enjoy your travels. This is why we don’t charge any fees on purchases 
+  while you’re abroad. We’ll even show you …"
       />
       <ArticleCard
-        image={currencyImage}
+        image={confettiImage}
         author="Claire Robinson"
-        title="Receive money in any currency with no fees"
-        text="The world is getting smaller and we’re becoming more mobile. So why should you be 
-  forced to only receive money in a single …"
+        title="Our invite-only Beta accounts are now live!"
+        text="After a lot of hard work by the whole team, we’re excited to launch our closed beta. 
+  It’s easy to request an invite through the site ..."
       />
     </CardContainer>
   </ArticlesContainer>
